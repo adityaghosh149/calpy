82edb36e7ce3a2dbadc0faa8e60ed96d111e0a7e
Ankush Mondal <mondalankush9851@gmail.com>
2025-04-24 10:20:54 +0530

    converted struct to class, add DType array, and cleanup

--- core/include/dtypes.hpp
+++ core/include/dtypes.hpp
@@ -1,6 +1,8 @@
 #ifndef DTYPES_HPP
 #define DTYPES_HPP
 
+#include <cstddef>
+
 typedef enum DType {
     UINT8,
     UINT16,
@@ -12,15 +14,29 @@ typedef enum DType {
     INT64,
     FLOAT32,
     FLOAT64
-};
+} DType;
 
-struct DTypeDesc {
+class DTypeDesc {
+public:
     const char *name; 
     DType type;
     size_t typeSize;
     size_t alignment;
-}
+};
 
-#endif 
+static DTypeDesc dTypeDesc[] = {
+    {"uint8_t",  UINT8,   1, 0},
+    {"uint16_t", UINT16,  2, 0},
+    {"uint32_t", UINT32,  4, 0},
+    {"uint64_t", UINT64,  8, 0},
+    {"int8_t",   INT8,    1, 0},
+    {"int16_t",  INT16,   2, 0},
+    {"int32_t",  INT32,   4, 0},
+    {"int64_t",  INT64,   8, 0},
+    {"float",    FLOAT32, 4, 0},
+    {"double",   FLOAT64, 8, 0}
+};
+
+#endif // DTYPES_HPP
 
 
